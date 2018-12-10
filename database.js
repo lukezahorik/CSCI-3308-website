ab6b2791085c42a031566f080e3331205380fb01
williewlchew <williewlchew@gmal.com>
2018-12-09 18:14:37 -0700

    yup2

--- database.js
+++ database.js
@@ -2,17 +2,14 @@
 
 var pgp = require('pg-promise')();
 
-const dbConfig = process.env.DATABASE_URL;
-/*
-{
-   host: 'localhost',
+const dbConfig = {
+   host: 'ec2-54-243-46-32.compute-1.amazonaws.com',
    port: 5432,
-   database: 'tipstest',
-   user: 'postgres',
-   password: 'wehc1234'
+   database: 'd7ncm4tt5a8edo',
+   user: 'fftbsmtlkjrdda',
+   password: '1f1ea62929ba0acbcbc74d4cd2267c04b70b1a2dfc1106c87aa440ec2ba70ee8'
 };
-*/
 
-var db =  pgp(dbConfig);
+var db = pgp(dbConfig);
 
 module.exports = db;
