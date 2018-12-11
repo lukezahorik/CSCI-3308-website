811671cdd2d2d31423cfe928edef6da74d23cebe
lukezahorik <lucaszahorik@gmail.com>
2018-12-11 15:36:04 -0700

    added static directory

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs');
 
 var expressValidator = require('express-validator');
 app.use(expressValidator());
+app.use( express.static( "img" ) );
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
